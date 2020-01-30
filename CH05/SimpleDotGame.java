67e7cb0ca7fa34eaf8dd90925ac6b1aee7e8da8d
Rashmit Pankhania <rashmitpankhania@gmail.com>
2020-01-30 17:35:18 +0530

    SimpleDotGame created but with bugs

--- CH05/SimpleDotGame.java
+++ CH05/SimpleDotGame.java
@@ -2,6 +2,23 @@ public class SimpleDotGame {
 	private int[] locationCells;
 	int numOfHits;
 
+	public static void main(String[] args){
+		int numOfGuesses = 0;
+		int startingCell = (int)(Math.random()*5);
+		int[] cells = {startingCell+2,startingCell+1,startingCell};
+		SimpleDotGame s = new SimpleDotGame();
+		s.setLocationCells(cells);
+
+		while(true){
+			String userInput = GameHelper.getUserInput("Enter a number:");
+			numOfGuesses++;
+			String result = s.checkYourself(userInput);
+			if(result == "kill")
+				break;
+		}
+		System.out.println("Your gueses are "+numOfGuesses);
+	}
+
 	String checkYourself(String userInput){
 		int guess = Integer.parseInt(userInput); 
 		String result = "miss";
