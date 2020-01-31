01f5f3e51ce62b7d2cc0b0da81b44772d3952a15
Rashmit Pankhania <rashmitpankhania@gmail.com>
2020-01-31 15:59:21 +0530

    finally game completed

--- CH06/DotGame.java
+++ CH06/DotGame.java
@@ -3,31 +3,29 @@ import java.util.ArrayList;
 public class DotGame {
 	private ArrayList<String> locationCells;
 	private String name;
-	
-	String checkYourself(String userInput){ 
-		String result = "miss";
 
-		for(String cell: locationCells){
-			int index = locationCells.indexOf(userInput);
-			if(index >= 0){
-				locationCells.remove(index);
-				if(locationCells.isEmpty())
-					result = "kill";
-				else
-					result = "hit";
-			}
+	String checkYourself(String userInput) {
+		String result = "miss";
+		int index = locationCells.indexOf(userInput);
+		if (index >= 0) {
+			locationCells.remove(index);
+			if (locationCells.isEmpty())
+				result = "kill";
+			else
+				result = "hit";
 		}
-		
-		System.out.println(result);
-		
 		return result;
 	}
 
-	void setLocationCells(ArrayList<String> l){
+	void setLocationCells(ArrayList<String> l) {
 		locationCells = l;
 	}
 
-	void setName(String i){
+	void setName(String i) {
 		name = i;
+	}
+
+	String getName() {
+		return name;
 	}
 }
--- CH06/notes.md
+++ CH06/notes.md
@@ -1,1 +1,12 @@
  to get any file from the different branch you can just checkout and give the relative path
+## sharpen your pencil
+1. declare and initialize
+2. call the setter in dot com method to set name
+3. print breif intr for the user
+4. repreat with each Dot com
+5. ask the helper for the location 
+6. call setter for the location
+7. as long as dot com is not empty
+8. getUserInput
+9. call checkuser guess method
+10. call finish game method 
