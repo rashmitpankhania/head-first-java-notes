feb78e4f695b897c37030975750ba7a6a9aaf9cd
Rashmit Pankhania <rashmitpankhania@gmail.com>
2020-02-19 19:13:25 +0530

    interface of action listener implemented

--- CH12/src/SimpleGui.java
+++ CH12/src/SimpleGui.java
@@ -1,14 +1,26 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SimpleGui {
+public class SimpleGui implements ActionListener {
+    JButton button;
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JButton button = new JButton("Click me!!");
+        SimpleGui gui = new SimpleGui();
+        gui.go();
+    }
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public void go() {
+        button = new JButton("click kar");
+        JFrame frame = new JFrame();
+        button.addActionListener(this);
         frame.getContentPane().add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
         frame.setVisible(true);
+    }
 
+    public void actionPerformed(ActionEvent actionEvent) {
+        button.setText("i m pressed");
     }
 }
