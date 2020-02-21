1abdd6243b04d60c84d8bb31b3d7adbf8a4476c1
Rashmit Pankhania <rashmitpankhania@gmail.com>
2020-02-21 19:54:03 +0530

    added more than two widgets on the frame and hooked the events together

--- CH12/src/SimpleGui.java
+++ CH12/src/SimpleGui.java
@@ -2,38 +2,53 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 
-public class SimpleGui implements ActionListener, ItemListener {
-    JButton button;
-    MyDrawPanel mypanel;
+public class SimpleGui {
+    JLabel label;
+    MyDrawPanel myPanel;
+
+    class ColorListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            myPanel.repaint();
+        }
+    }
+
+    class LabelListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            label.setText("label changed");
+        }
+    }
+
+    public void go() {
+        JButton colorButton = new JButton("change color");
+        JButton labelButton = new JButton("change label");
+
+        label = new JLabel("some cliche label");
+        myPanel = new MyDrawPanel();
+
+        JFrame frame = new JFrame("RASH");
+
+        colorButton.addActionListener(new ColorListener());
+        labelButton.addActionListener(new LabelListener());
+
+        frame.getContentPane().add(BorderLayout.EAST, labelButton);
+        frame.getContentPane().add(BorderLayout.WEST, label);
+        frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
+        frame.getContentPane().add(BorderLayout.CENTER, myPanel);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
 
     public static void main(String[] args) {
         SimpleGui gui = new SimpleGui();
         gui.go();
     }
 
-    public void go() {
-        button = new JButton("click kar");
-        mypanel = new MyDrawPanel();
-        JFrame frame = new JFrame();
-        button.addActionListener(this);
-        button.addItemListener(this);
-        frame.getContentPane().add(BorderLayout.SOUTH, button);
-        frame.getContentPane().add(BorderLayout.CENTER, mypanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
-        frame.setVisible(true);
-    }
-
-    public void actionPerformed(ActionEvent actionEvent) {
-        button.setText("i m pressed");
-        mypanel.repaint();
-    }
-
-    @Override
-    public void itemStateChanged(ItemEvent itemEvent) {
-        button.setText("helo");
-    }
 }
