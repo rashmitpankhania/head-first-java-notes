8b3daf67cea29877f5177081447b252e1e37fd5c
Rashmit Pankhania <rashmitpankhania@gmail.com>
2020-02-24 17:59:55 +0530

    fresh start again

--- CH13/src/Button1.java
+++ CH13/src/Button1.java
@@ -2,11 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 
-public class Button1  implements ItemListener {
-    JCheckBox checkBox = new JCheckBox("e check box");
+public class Button1 {
     public static void main(String[] args) {
         Button1 button1 = new Button1();
         button1.go();
@@ -16,17 +13,7 @@ public class Button1  implements ItemListener {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.getContentPane().add(BorderLayout.CENTER, checkBox);
-        checkBox.addItemListener(this);
-
         frame.setSize(300,300);
         frame.setVisible(true);
-    }
-
-    @Override
-    public void itemStateChanged(ItemEvent itemEvent) {
-        String on = "off";
-        if(checkBox.isSelected()) on = "on";
-        System.out.println("it is "+on);
     }
 }
