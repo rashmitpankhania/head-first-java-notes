a08e68684f77eb01ed2288c49d1cb147fdd49e70
Rashmit Pankhania <rashmitpankhania@gmail.com>
2020-02-24 17:59:55 +0530

    fresh start

--- CH13/src/Button1.java
+++ CH13/src/Button1.java
@@ -3,9 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Button1 implements ActionListener {
-    JTextArea textArea = new JTextArea(10,20);
-
+public class Button1 {
     public static void main(String[] args) {
         Button1 button1 = new Button1();
         button1.go();
@@ -15,27 +13,7 @@ public class Button1 implements ActionListener {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel();
-        frame.getContentPane().add(BorderLayout.CENTER, panel);
-        panel.setBackground(Color.yellow);
-        textArea.setLineWrap(true);
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-
-        panel.add(scrollPane);
-
-        JButton button = new JButton("Click");
-        frame.getContentPane().add(BorderLayout.SOUTH, button);
-        button.addActionListener(this);
-
-
         frame.setSize(300,300);
         frame.setVisible(true);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
-        textArea.append("Button clicked \n");
     }
 }
