aabf427bb5c492e4e5f9c58f9443c17e48ffdd21
Rashmit Pankhania <rashmitpankhania@gmail.com>
2020-02-24 18:07:50 +0530

    Check box working

--- CH13/src/Button1.java
+++ CH13/src/Button1.java
@@ -2,8 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class Button1 {
+public class Button1  implements ItemListener {
+    JCheckBox checkBox = new JCheckBox("e check box");
     public static void main(String[] args) {
         Button1 button1 = new Button1();
         button1.go();
@@ -13,7 +16,17 @@ public class Button1 {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        frame.getContentPane().add(BorderLayout.CENTER, checkBox);
+        checkBox.addItemListener(this);
+
         frame.setSize(300,300);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent itemEvent) {
+        String on = "off";
+        if(checkBox.isSelected()) on = "on";
+        System.out.println("it is "+on);
     }
 }
