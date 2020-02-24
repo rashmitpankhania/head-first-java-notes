bee4d98799936a5ef689ea1199bff01cafe35c56
Rashmit Pankhania <rashmitpankhania@gmail.com>
2020-02-24 17:56:09 +0530

    JtextArea working

--- CH13/src/Button1.java
+++ CH13/src/Button1.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Button1 {
+public class Button1 implements ActionListener {
+    JTextArea textArea = new JTextArea(10,20);
+
     public static void main(String[] args) {
         Button1 button1 = new Button1();
         button1.go();
@@ -10,23 +14,28 @@ public class Button1 {
     void go(){
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel();
 
-        frame.getContentPane().add(BorderLayout.EAST, panel);
-        panel.setBackground(Color.darkGray);
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        JButton button = new JButton("shock me");
-        JButton buttonTwo = new JButton("bliss");
-        JButton buttonThree = new JButton("huh?");
-        panel.add(button);
-        panel.add(buttonTwo);
-        panel.add(buttonThree);
-        JPanel centerPanel = new JPanel();
-        centerPanel.add(new JTextField("name likh bhsodik"));
-        centerPanel.setBackground(Color.cyan);
-        frame.getContentPane().add(BorderLayout.CENTER, centerPanel);
+        JPanel panel = new JPanel();
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        panel.setBackground(Color.yellow);
+        textArea.setLineWrap(true);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+        panel.add(scrollPane);
+
+        JButton button = new JButton("Click");
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        button.addActionListener(this);
+
 
         frame.setSize(300,300);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        textArea.append("Button clicked \n");
     }
 }
